fafefab6da95e0a0502165c56c4de290163fb7e7
Adam Gyore <adam.gyore@gmail.com>
2021-01-30 12:43:36 +0100

    Refactor the CORS header for StockInfo

--- src/main/java/com/codecool/stockexchange/controller/StockInfoController.java
+++ src/main/java/com/codecool/stockexchange/controller/StockInfoController.java
@@ -1,31 +1,29 @@
 package com.codecool.stockexchange.controller;
 
-import javax.servlet.http.HttpServletResponse;
-
 import com.codecool.stockexchange.apimodel.ChartDataPoint;
 import com.codecool.stockexchange.apimodel.Quote;
 import com.codecool.stockexchange.service.StockInfoService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class StockInfoController {
 
     @Autowired
     StockInfoService stockInfoService;
 
     @GetMapping("/quote/{symbol}")
-    public Quote getStockData(@PathVariable String symbol, HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
+    public Quote getStockData(@PathVariable String symbol) {
         return stockInfoService.findFirstBySymbol(symbol);
     }
 
     @GetMapping("/chart/{symbol}")
-    public ChartDataPoint[] getChartData(@PathVariable String symbol, HttpServletResponse response) {
-        response.addHeader("Access-Control-Allow-Origin", "*");
+    public ChartDataPoint[] getChartData(@PathVariable String symbol) {
         return stockInfoService.getChartDataPoints(symbol);
     }
 }
