4f411ef52e2985c204dc6a8f7fd96ffd3c08bb68
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-30 16:15:24 +0100

    Create NewsItem entity - does not store summary yet

---
+++ src/main/java/com/codecool/stockexchange/entity/NewsItem.java
@@ -0,0 +1,47 @@
+package com.codecool.stockexchange.entity;
+
+import com.codecool.stockexchange.apimodel.NewsItemAPI;
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Builder
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
+public class NewsItem {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String image;
+    private String url;
+    private String headline;
+    @Column(length = 1000)
+    private String summary;
+    private String source;
+    private long datetime;
+    private String lang;
+
+    @ManyToOne
+    private StockInfo stockInfo;
+
+    public NewsItem(NewsItemAPI news){
+        this.image = news.getImage();
+        this.url = news.getUrl();
+        this.headline = news.getHeadline();
+//        this.summary = news.getSummary();
+        this.source = news.getSource();
+        this.datetime = news.getDatetime();
+        this.lang = news.getLang();
+    }
+
+}
