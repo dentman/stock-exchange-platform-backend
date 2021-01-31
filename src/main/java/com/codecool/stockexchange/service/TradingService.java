06977917669f604ef1b74986a56c7ce45cd356b9
FruzsinaElekes <elekes.fr@gmail.com>
2021-01-31 16:41:14 +0100

    Add Status-es, schema for tradingService actions

--- src/main/java/com/codecool/stockexchange/service/TradingService.java
+++ src/main/java/com/codecool/stockexchange/service/TradingService.java
@@ -7,7 +7,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class TradingService {
     public Status handleOrder(OrderItem order) {
+        // validate: whatever it means
+        // check if status is pending
+        // check price of symbol vs limitPrice-> succ/fail
+        // BUY: check user account balance vs amount*price -> succ/fail
+        // SELL: check user stock balance vs amount -> succ/fail
+
+
         System.out.println(order);
+        return handleTransaction(order);
+    }
+
+    private Status handleTransaction(OrderItem order) {
+        // -> create Transaction
+        // modify db:
+        // portfolio_item (new item/update):
+        // account (update)
         return Status.COMPLETED;
     }
+
+
 }
