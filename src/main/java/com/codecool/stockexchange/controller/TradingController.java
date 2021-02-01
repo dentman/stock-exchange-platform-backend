c788afb4ad67f4ac23ae9e6df4ebd01b41ff0316
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-01 12:01:25 +0100

    Add CrossOrigin annotation to TradingController

--- src/main/java/com/codecool/stockexchange/controller/TradingController.java
+++ src/main/java/com/codecool/stockexchange/controller/TradingController.java
@@ -5,11 +5,13 @@ import com.codecool.stockexchange.entity.trade.OrderStatus;
 import com.codecool.stockexchange.service.TradingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TradingController {
 
     @Autowired
