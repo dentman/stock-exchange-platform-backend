62f455644c2bbe097eaa7cc44e7a7728edde7257
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-02 13:08:26 +0100

    Fix identification of user when order is placed

--- src/main/java/com/codecool/stockexchange/controller/TradingController.java
+++ src/main/java/com/codecool/stockexchange/controller/TradingController.java
@@ -2,15 +2,15 @@ package com.codecool.stockexchange.controller;
 
 import com.codecool.stockexchange.entity.trade.Order;
 import com.codecool.stockexchange.entity.trade.OrderStatus;
+import com.codecool.stockexchange.entity.user.User;
+import com.codecool.stockexchange.repository.UserRepository;
 import com.codecool.stockexchange.service.TradingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin
@@ -19,9 +19,16 @@ public class TradingController {
     @Autowired
     TradingService tradingService;
 
-    @PostMapping("/trade")
-    public OrderStatus postOrder(@RequestBody @Validated Order order){
-        order.setDate(LocalDateTime.now());
-        return tradingService.handleOrder(order);
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping("/trade/{user_id}")
+    public OrderStatus postOrder(@PathVariable Long user_id, @RequestBody @Validated Order order){
+        Optional<User> userOptional = userRepository.findById(user_id);
+        if (userOptional.isPresent()){
+            order.setUser(userOptional.get());
+            order.setDate(LocalDateTime.now());
+            return tradingService.handleOrder(order);
+        } else return null;
     }
 }
