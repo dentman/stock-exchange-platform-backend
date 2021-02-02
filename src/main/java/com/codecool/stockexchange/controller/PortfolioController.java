9e0604655de17f82e1bd481a79e14dd21aa49140
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-02 13:12:54 +0100

    Add Controller to request transactions for user

---
+++ src/main/java/com/codecool/stockexchange/controller/PortfolioController.java
@@ -0,0 +1,25 @@
+package com.codecool.stockexchange.controller;
+
+import com.codecool.stockexchange.entity.trade.StockTransaction;
+import com.codecool.stockexchange.service.PortfolioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PortfolioController {
+
+    @Autowired
+    PortfolioService portfolioService;
+
+    @GetMapping("/transactions/{user_id}")
+    public List<StockTransaction> getTransactionsForUser(@PathVariable("user_id") Long user_id){
+        return portfolioService.getTransactionsForUser(user_id);
+    }
+
+
+
+}
