42cf7138091cc7cbdfa637168c970534ee552b5d
Adam Gyore <adam.gyore@gmail.com>
2021-02-06 17:44:18 +0100

    Add RefreshInterval constant to StockPriceUpdateService

--- src/main/java/com/codecool/stockexchange/service/update/StockPriceUpdateService.java
+++ src/main/java/com/codecool/stockexchange/service/update/StockPriceUpdateService.java
@@ -3,6 +3,7 @@ package com.codecool.stockexchange.service.update;
 import com.codecool.stockexchange.entity.stock.Stock;
 import com.codecool.stockexchange.repository.StockRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,11 +14,13 @@ import java.util.List;
 @Service
 public class StockPriceUpdateService {
 
+    private final int REFRESH_INTERVAL = 5000;
+
     @Autowired
     private StockRepository stockRepository;
 
 
-    @Scheduled(initialDelay = 5000, fixedRate = 5000)
+    @Scheduled(initialDelay = REFRESH_INTERVAL, fixedRate = REFRESH_INTERVAL)
     @Transactional
     public void simulateStockPrices() {
         List<Stock> stockData = stockRepository.findAll();
