29fa74dda5b25720e60ae31a85ccaa9c7f43cc3d
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-15 10:35:28 +0100

    Add method to persist new user, if username is unique

--- src/main/java/com/codecool/stockexchange/service/UserService.java
+++ src/main/java/com/codecool/stockexchange/service/UserService.java
@@ -1,9 +1,21 @@
 package com.codecool.stockexchange.service;
 
+import com.codecool.stockexchange.entity.Message;
+import com.codecool.stockexchange.entity.user.Account;
 import com.codecool.stockexchange.entity.user.User;
+import com.codecool.stockexchange.exception.user.UserExistsException;
 import com.codecool.stockexchange.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -11,7 +23,24 @@ public class UserService {
     @Autowired
     UserRepository userRepository;
 
+    PasswordEncoder pwe = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+
     public User findUserById(Long user_id) {
         return userRepository.findUserById(user_id);
+    }
+
+    @Transactional
+    public User saveNewUser(User user) {
+        Optional<User> userOptional = userRepository.findByUsername(user.getUsername());
+        if (userOptional.isEmpty()){
+            Account account = Account.builder().balance(BigDecimal.valueOf(10000)).currency("USD").build();
+            account.setUser(user);
+            user.setPassword(pwe.encode(user.getPassword()));
+            user.setAccount(account);
+            userRepository.save(user);
+            return user;
+        }
+        throw new UserExistsException("Username " + user.getUsername() + " already exists");
     }
 }
