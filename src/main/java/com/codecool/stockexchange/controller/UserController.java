98e8388361c838a052359e1b22ce548c0cb7d771
Adam Gyore <adam.gyore@gmail.com>
2021-02-17 23:15:25 +0100

    Remove path variable from UserController

--- src/main/java/com/codecool/stockexchange/controller/UserController.java
+++ src/main/java/com/codecool/stockexchange/controller/UserController.java
@@ -1,11 +1,12 @@
 package com.codecool.stockexchange.controller;
 
 import com.codecool.stockexchange.entity.user.User;
+import com.codecool.stockexchange.security.CustomUser;
 import com.codecool.stockexchange.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,9 +16,10 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/user/{user_id}")
-    public User getUserById(@PathVariable Long user_id){
-        return userService.findUserById(user_id);
+    @GetMapping("/user")
+    public User getUserById(){
+        CustomUser user = (CustomUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return userService.findUserById(user.getUserId());
     }
 
 }
