a9ec173582429abcfd8c4187e9a227c0d72b1deb
Adam Gyore <adam.gyore@gmail.com>
2021-02-21 19:22:13 +0100

    Add websocket configuration class

---
+++ src/main/java/com/codecool/stockexchange/WebSocketConfiguration.java
@@ -0,0 +1,22 @@
+package com.codecool.stockexchange;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/socket").setAllowedOrigins("http://localhost:3000");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry brokerRegistry) {
+        brokerRegistry.setApplicationDestinationPrefixes("/app");
+    }
+
+}
