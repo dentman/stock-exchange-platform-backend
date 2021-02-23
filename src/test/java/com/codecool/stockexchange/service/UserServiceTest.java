10c88d4ff839d682b3a2ea3221b55db5ed6a7c49
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-23 14:33:50 +0100

    Add test for saveNewUser() throws UserExistsException

--- src/test/java/com/codecool/stockexchange/service/UserServiceTest.java
+++ src/test/java/com/codecool/stockexchange/service/UserServiceTest.java
@@ -1,5 +1,7 @@
 package com.codecool.stockexchange.service;
 
+import com.codecool.stockexchange.StockExchangeApplication;
+import com.codecool.stockexchange.entity.user.Role;
 import com.codecool.stockexchange.entity.user.User;
 import com.codecool.stockexchange.exception.user.UserExistsException;
 import com.codecool.stockexchange.repository.UserRepository;
@@ -10,17 +12,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-@DataJpaTest
-@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@AutoConfigureTestDatabase
+@SpringBootTest(classes = StockExchangeApplication.class)
 class UserServiceTest {
 
     @Autowired
@@ -32,12 +36,14 @@ class UserServiceTest {
 
     @Test
     public void saveNewUserThrowsUserExistsException() {
+        String username = "test@user.hu";
+        userRepository.save(User.builder()
+                                .username(username)
+                                .password("string")
+                                .roles(List.of(Role.ROLE_USER))
+                                .build());
         User mockUser = mock(User.class);
-        when(mockUser.getUsername()).thenReturn("test@user.hu");
-
-        User mockUserStored = mock(User.class);
-        when(mockUserStored.getUsername()).thenReturn("test@user.hu");
-        userRepository.save(mockUserStored);
+        when(mockUser.getUsername()).thenReturn(username);
 
         assertThrows(UserExistsException.class, () -> userService.saveNewUser(mockUser));
 
