5b024ecbbdbf2284317247da070885cd19ea18a3
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-23 15:12:49 +0100

    Add test for saveNewUser() returning valid User

--- src/test/java/com/codecool/stockexchange/service/UserServiceTest.java
+++ src/test/java/com/codecool/stockexchange/service/UserServiceTest.java
@@ -5,6 +5,8 @@ import com.codecool.stockexchange.entity.user.Role;
 import com.codecool.stockexchange.entity.user.User;
 import com.codecool.stockexchange.exception.user.UserExistsException;
 import com.codecool.stockexchange.repository.UserRepository;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.junit.MockitoJUnitRunner;
@@ -16,6 +18,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -28,24 +31,30 @@ import static org.mockito.Mockito.when;
 class UserServiceTest {
 
     @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    UserService userService;
+    private UserService userService;
+    private String username = "test@user.hu";
 
 
     @Test
+    @Order(1)
+    public void saveNewUserReturnsUser(){
+        User toSave = User.builder()
+                .username(username)
+                .password("plainTextPassword")
+                .roles(List.of())
+                .build();
+        User saved = userService.saveNewUser(toSave);
+        assertNotNull(saved.getId());
+        assertEquals(Role.ROLE_USER ,saved.getRoles().get(0));
+        assertTrue(saved.getAccount().getBalance().equals(BigDecimal.valueOf(10000)));
+    }
+
+    @Test
+    @Order(2)
     public void saveNewUserThrowsUserExistsException() {
-        String username = "test@user.hu";
-        userRepository.save(User.builder()
-                                .username(username)
-                                .password("string")
-                                .roles(List.of(Role.ROLE_USER))
-                                .build());
         User mockUser = mock(User.class);
         when(mockUser.getUsername()).thenReturn(username);
 
         assertThrows(UserExistsException.class, () -> userService.saveNewUser(mockUser));
-
     }
 }
