848fce51298a4069b6a849863fe8b0fee4a81a50
FruzsinaElekes <elekes.fr@gmail.com>
2021-02-23 10:42:50 +0100

    Delete handler for InvalidParameterException

--- src/main/java/com/codecool/stockexchange/exception/ControllerExceptionHandler.java
+++ src/main/java/com/codecool/stockexchange/exception/ControllerExceptionHandler.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.security.InvalidParameterException;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
@@ -41,14 +40,6 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(NumberFormatException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorMessage handleInvalidNumberFormat(NumberFormatException e) { return new ErrorMessage(e); }
-
-    @ExceptionHandler(InvalidParameterException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorMessage handleMessageNotReadable(InvalidParameterException e) {
-        return new ErrorMessage(
-                "Invalid data provided for the order. Please check the selected paramaters!",
-                e.getClass().getSimpleName());
-    }
 
     @ExceptionHandler(UserExistsException.class)
     @ResponseStatus(HttpStatus.CONFLICT)
