33c3a40fe725e73002a9eb7d6e4f0ebc8cb666e3
Adam Gyore <adam.gyore@gmail.com>
2021-03-02 20:09:39 +0100

    Remove unused  rest controller

--- api-service/src/main/java/com/codecool/apiservice/controller/UpdateController.java
+++ api-service/src/main/java/com/codecool/apiservice/controller/UpdateController.java
@@ -3,32 +3,25 @@ package com.codecool.apiservice.controller;
 import com.codecool.apiservice.resultmodel.Stock;
 import com.codecool.apiservice.service.update.StockUpdateService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Mono;
 
-@RestController
+@Controller
 public class UpdateController {
 
     @Autowired
     private StockUpdateService stockUpdateService;
 
-    @GetMapping("/create/{symbol}")
-    public Stock createNewStock(@PathVariable String symbol) {
-
-        Stock stock = stockUpdateService.createNewStock(symbol);
-        System.out.println(stock);
-        return stock;
+    @MessageMapping("create")
+    Mono<Stock> createStock (String symbol) {
+        Stock out = stockUpdateService.createNewStock(symbol);
+        return Mono.just(out);
     }
 
-    @GetMapping("/update")
-    public Stock updateStock(@RequestBody Stock stock) {
-
-        System.out.println(stock);
+    @MessageMapping("update")
+    Mono<Stock> updateStock (Stock stock) {
         Stock out = stockUpdateService.updateStock(stock);
-        return out;
+        return Mono.just(out);
     }
-
-
 }
