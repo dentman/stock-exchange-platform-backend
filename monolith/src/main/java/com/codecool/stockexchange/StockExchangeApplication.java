68dddd962613d77ecbafea25f1234d076dad3992
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-02 12:19:55 +0100

    Add details to properties files

--- monolith/src/main/java/com/codecool/stockexchange/StockExchangeApplication.java
+++ monolith/src/main/java/com/codecool/stockexchange/StockExchangeApplication.java
@@ -18,6 +18,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -25,6 +26,7 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableScheduling
 public class StockExchangeApplication {
 
