dae2afafcefd8bf7134524d918141b873dd566e4
Adam Gyore <adam.gyore@gmail.com>
2021-03-06 21:09:42 +0100

    Add filter for invalid route requests

--- src/main/java/com/codecool/stockexchange/configuration/SecurityConfiguration.java
+++ src/main/java/com/codecool/stockexchange/configuration/SecurityConfiguration.java
@@ -1,6 +1,6 @@
 package com.codecool.stockexchange.configuration;
 
-import com.codecool.stockexchange.security.JwtRequestFilter;
+import com.codecool.stockexchange.filter.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
---
+++ src/main/java/com/codecool/stockexchange/filter/RequestRouteFilter.java
@@ -0,0 +1,44 @@
+package com.codecool.stockexchange.filter;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
+
+@Component
+
+public class RequestRouteFilter extends GenericFilterBean {
+
+    @Override
+    public void doFilter(ServletRequest request,
+                         ServletResponse response,
+                         FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        String contentType = request.getContentType();
+        String requestURI = req.getRequestURI();
+
+        if (isAllowedRoute(requestURI, contentType)) {
+            chain.doFilter(request, response);
+            return;
+        }
+        request.getRequestDispatcher("/").forward(request, response);
+    }
+
+    private boolean isAllowedRoute(String URI, String contentType) {
+        List<String> notRedirectedList = List.of(
+                "/stock-base-data/list",
+                "/about/en",
+                "/socket",
+                "/favicon.ico"
+        );
+        boolean isAllowed = contentType != null && contentType.startsWith("application/json") ||
+                URI.equals("/") ||
+                notRedirectedList.contains(URI) ||
+                URI.startsWith("/static");
+        return isAllowed;
+    }
+}
