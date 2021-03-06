9830f14219ca7a4455c4d007da4ad9aed43bd5dc
Adam Gyore <adam.gyore@gmail.com>
2021-03-06 17:38:13 +0100

    Move configuration classes to config folder

--- src/main/java/com/codecool/stockexchange/configuration/WebSocketConfiguration.java
+++ src/main/java/com/codecool/stockexchange/configuration/WebSocketConfiguration.java
@@ -1,4 +1,4 @@
-package com.codecool.stockexchange;
+package com.codecool.stockexchange.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
